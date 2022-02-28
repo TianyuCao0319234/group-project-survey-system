3572a6884c31ededcc5495cfda96c7850ff990cd
Tianyu <tcao@upei.ca>
2022-02-27 23:13:17 -0500

    add survey list

---
+++ survey-system/app/Models/Profile.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+    public $table = 'profiles';
+    protected $fillable = [
+        'first_name',
+        'email',
+        'password',
+        'phone',
+        'DOB',
+        'last_name',
+        'address'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    public $timestamps = false;
+
+}
---
+++ survey-system/app/Models/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    use HasFactory;
+    public $table = 'profiles';
+    protected $fillable = [
+        'first_name',
+        'email',
+        'password',
+        'phone',
+        'DOB',
+        'last_name',
+        'address'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    public $timestamps = false;
+}
